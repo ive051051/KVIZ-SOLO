e33b730a1b190a3b6c9647674a17150a5f257581
ive051051 <ivek05171@gmail.com>
2026-04-08 16:52:59 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "kviz-igra-v39";
+const CACHE_NAME = "kviz-igra-v41";
 
 const ASSETS_TO_CACHE = [
   "./",
@@ -7,19 +7,24 @@ const ASSETS_TO_CACHE = [
   "./service-worker.js",
   "./pozadina.png",
   "./START.png",
+  "./pobijednik.png",
   "./icon-192.png",
   "./icon-512.png",
   "./applause.mp3",
   "./boo.mp3"
 ];
 
+// INSTALL
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS_TO_CACHE))
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
   );
   self.skipWaiting();
 });
 
+// ACTIVATE
 self.addEventListener("activate", event => {
   event.waitUntil(
     caches.keys().then(keys =>
@@ -35,12 +40,38 @@ self.addEventListener("activate", event => {
   self.clients.claim();
 });
 
+// FETCH
 self.addEventListener("fetch", event => {
   if (event.request.method !== "GET") return;
 
+  const request = event.request;
+
+  if ((request.headers.get("accept") || "").includes("text/html")) {
+    event.respondWith(
+      fetch(request)
+        .then(networkResponse => {
+          return caches.open(CACHE_NAME).then(cache => {
+            cache.put(request, networkResponse.clone());
+            return networkResponse;
+          });
+        })
+        .catch(() => caches.match(request))
+    );
+    return;
+  }
+
   event.respondWith(
-    caches.match(event.request).then(cached => {
-      return cached || fetch(event.request);
+    caches.match(request).then(cached => {
+      const fetchPromise = fetch(request)
+        .then(networkResponse => {
+          return caches.open(CACHE_NAME).then(cache => {
+            cache.put(request, networkResponse.clone());
+            return networkResponse;
+          });
+        })
+        .catch(() => cached);
+
+      return cached || fetchPromise;
     })
   );
 });
